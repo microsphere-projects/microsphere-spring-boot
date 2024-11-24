676daaba603ec7cea526cc74dfb480feb00633ce
Mercy Ma <mercyblitz@gmail.com>
2024-11-24 22:41:52 +0800

    Create OriginTrackedConfigurationPropertyInitializer.java

---
+++ microsphere-core-spring-boot-starter/src/main/java/io/microsphere/spring/boot/env/config/OriginTrackedConfigurationPropertyInitializer.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.spring.boot.env.config;
+
+import io.microsphere.spring.context.event.BeanListenerAdapter;
+import io.microsphere.spring.util.BeanRegistrar;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.boot.env.OriginTrackedMapPropertySource;
+import org.springframework.boot.origin.Origin;
+import org.springframework.boot.origin.OriginLookup;
+import org.springframework.boot.origin.OriginTrackedValue;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static io.microsphere.spring.util.BeanRegistrar.registerBean;
+
+/**
+ * {@link ApplicationContextInitializer} class supports origin tracked configuration property.
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ConfigurableEnvironment
+ * @since ApplicationContextInitializer
+ */
+public class OriginTrackedConfigurationPropertyInitializer extends BeanListenerAdapter
+        implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    public static final String BEAN_NAME = "originTrackedConfigurationPropertyInitializer";
+
+    private boolean initialized = false;
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+        BeanDefinitionRegistry registry = (BeanDefinitionRegistry) beanFactory;
+        registerBean(registry, BEAN_NAME, this);
+    }
+
+    @Override
+    public void onBeforeBeanInstantiate(String beanName, RootBeanDefinition mergedBeanDefinition) {
+        if (initialized) {
+            return;
+        }
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        MutablePropertySources propertySources = environment.getPropertySources();
+        initializePropertySources(propertySources);
+        initialized = true;
+    }
+
+    private void initializePropertySources(MutablePropertySources propertySources) {
+        for (PropertySource propertySource : propertySources) {
+            if (isPropertySourceCandidate(propertySource)) {
+                String name = propertySource.getName();
+                PropertySource originTrackedPropertySource = createOriginTrackedPropertySource(propertySource);
+                propertySources.replace(name, originTrackedPropertySource);
+            }
+        }
+    }
+
+    private boolean isPropertySourceCandidate(PropertySource propertySource) {
+        return (propertySource instanceof EnumerablePropertySource<?>) &&
+                !(propertySource instanceof OriginLookup);
+    }
+
+    private PropertySource createOriginTrackedPropertySource(PropertySource propertySource) {
+        EnumerablePropertySource enumerablePropertySource = (EnumerablePropertySource) propertySource;
+        String[] propertyNames = enumerablePropertySource.getPropertyNames();
+        int size = propertyNames.length;
+        Map<String, Object> source = new LinkedHashMap<>(size);
+        for (int i = 0; i < size; i++) {
+            String propertyName = propertyNames[i];
+            Object propertyValue = enumerablePropertySource.getProperty(propertyName);
+            if (propertyValue instanceof OriginTrackedValue) {
+                continue;
+            }
+            Origin origin = resolveOrigin(propertySource);
+            // propertyValue with origin
+            propertyValue = OriginTrackedValue.of(propertyValue, origin);
+            source.put(propertyName, propertyValue);
+        }
+        return new OriginTrackedMapPropertySource(propertySource.getName(), source);
+    }
+
+    private Origin resolveOrigin(PropertySource propertySource) {
+        // TODO
+        return new NamedOrigin(propertySource.getName());
+    }
+
+    static class NamedOrigin implements Origin {
+
+        private final String name;
+
+        NamedOrigin(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+}
