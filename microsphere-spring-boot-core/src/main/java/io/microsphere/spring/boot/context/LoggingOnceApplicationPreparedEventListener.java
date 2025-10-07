d4ad2cad3c0d9377fbbb42fc27b5607f1f89d679
Mercy Ma <mercyblitz@gmail.com>
2025-10-07 13:53:09 +0800

    Update LoggingOnceApplicationPreparedEventListener.java

--- microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/context/LoggingOnceApplicationPreparedEventListener.java
+++ microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/context/LoggingOnceApplicationPreparedEventListener.java
@@ -17,17 +17,12 @@
 
 package io.microsphere.spring.boot.context;
 
-import io.microsphere.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
-import static io.microsphere.logging.LoggerFactory.getLogger;
 import static io.microsphere.spring.boot.constants.PropertyConstants.DEFAULT_MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL;
-import static io.microsphere.spring.boot.constants.PropertyConstants.MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL_PROPERTY_NAME;
-import static io.microsphere.util.ArrayUtils.arrayToString;
-import static io.microsphere.util.ArrayUtils.ofArray;
-import static java.util.Locale.ENGLISH;
+import static io.microsphere.spring.boot.util.SpringApplicationUtils.getLoggingLevel;
+import static io.microsphere.spring.boot.util.SpringApplicationUtils.log;
 
 /**
  * {@link OnceApplicationPreparedEventListener} class for logging with specified level.
@@ -38,15 +33,13 @@ import static java.util.Locale.ENGLISH;
  */
 public class LoggingOnceApplicationPreparedEventListener extends OnceApplicationPreparedEventListener {
 
-    private static final Logger logger = getLogger(LoggingOnceApplicationPreparedEventListener.class);
-
     public LoggingOnceApplicationPreparedEventListener() {
         super.setOrder(LOWEST_PRECEDENCE);
     }
 
     @Override
     protected void onApplicationEvent(SpringApplication springApplication, String[] args, ConfigurableApplicationContext context) {
-        log(springApplication, args, context);
+        log(springApplication, context, args);
     }
 
     @Override
@@ -54,60 +47,4 @@ public class LoggingOnceApplicationPreparedEventListener extends OnceApplication
         String level = getLoggingLevel(context);
         return DEFAULT_MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL.equals(level);
     }
-
-    String getLoggingLevel(ConfigurableApplicationContext context) {
-        Environment environment = context.getEnvironment();
-        String level = environment.getProperty(MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL_PROPERTY_NAME, DEFAULT_MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL);
-        return level.toLowerCase(ENGLISH);
-    }
-
-    void log(SpringApplication springApplication, String[] args, ConfigurableApplicationContext context) {
-        String messagePattern =
-                "SpringApplication: " +
-                        "    main class : '{}' ," +
-                        "    web type : '{}' ," +
-                        "    sources : {} ," +
-                        "    all sources : {} ," +
-                        "    additional profiles : {} ," +
-                        "    initializers : {} ," +
-                        "    listeners : {}," +
-                        "    args : {}," +
-                        "    context id : '{}'";
-
-        Object[] arguments = ofArray(springApplication.getMainApplicationClass(),
-                springApplication.getWebApplicationType(),
-                springApplication.getSources(),
-                springApplication.getAllSources(),
-                springApplication.getAdditionalProfiles(),
-                springApplication.getInitializers(),
-                springApplication.getListeners(),
-                arrayToString(args),
-                context.getId());
-
-        String level = getLoggingLevel(context);
-
-        switch (level) {
-            case "trace": {
-                logger.trace(messagePattern, arguments);
-                break;
-            }
-            case "debug": {
-                logger.debug(messagePattern, arguments);
-                break;
-            }
-            case "info": {
-                logger.info(messagePattern, arguments);
-                break;
-            }
-            case "warn": {
-                logger.warn(messagePattern, arguments);
-                break;
-            }
-            case "error": {
-                logger.error(messagePattern, arguments);
-                break;
-            }
-        }
-    }
-
 }
