ea23d8d0c13a615983b788d4600ba39cc204841c
Mercy Ma <mercyblitz@gmail.com>
2025-10-07 13:53:05 +0800

    Update SpringApplicationUtils.java

--- microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/util/SpringApplicationUtils.java
+++ microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/util/SpringApplicationUtils.java
@@ -1,6 +1,10 @@
 package io.microsphere.spring.boot.util;
 
+import io.microsphere.logging.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.PropertyResolver;
+import org.springframework.core.env.PropertySources;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.ResourceLoader;
 
@@ -8,6 +12,12 @@ import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import static io.microsphere.logging.LoggerFactory.getLogger;
+import static io.microsphere.spring.boot.constants.PropertyConstants.DEFAULT_MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL;
+import static io.microsphere.spring.boot.constants.PropertyConstants.MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL_PROPERTY_NAME;
+import static io.microsphere.util.ArrayUtils.arrayToString;
+import static io.microsphere.util.ArrayUtils.ofArray;
+import static java.util.Locale.ENGLISH;
 import static org.springframework.util.StringUtils.hasText;
 
 /**
@@ -17,6 +27,8 @@ import static org.springframework.util.StringUtils.hasText;
  * @since 1.0.0
  */
 public abstract class SpringApplicationUtils {
+
+    private static final Logger logger = getLogger(SpringApplicationUtils.class);
 
     private static final Set<String> defaultPropertiesResources = new LinkedHashSet<>();
 
@@ -62,5 +74,86 @@ public abstract class SpringApplicationUtils {
             resourceLoader = new DefaultResourceLoader(springApplication.getClassLoader());
         }
         return resourceLoader;
+    }
+
+    /**
+     * Get logging level with upper case from the Spring {@link PropertySources}
+     *
+     * @param context {@link ConfigurableApplicationContext} context
+     * @return logging level with upper case
+     */
+    public static String getLoggingLevel(ConfigurableApplicationContext context) {
+        return getLoggingLevel(context.getEnvironment());
+    }
+
+    /**
+     * Get logging level(Upper case) from the Spring {@link PropertySources}
+     *
+     * @param propertyResolver {@link PropertyResolver}
+     * @return logging level with upper case
+     */
+    public static String getLoggingLevel(PropertyResolver propertyResolver) {
+        String level = propertyResolver.getProperty(MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL_PROPERTY_NAME, DEFAULT_MICROSPHERE_SPRING_BOOT_LOGGING_LEVEL);
+        return level.toUpperCase(ENGLISH);
+    }
+
+    /**
+     * Log {@link SpringApplication}
+     *
+     * @param springApplication {@link SpringApplication}
+     * @param context           {@link ConfigurableApplicationContext}
+     * @param args              the command line arguments
+     */
+    public static void log(SpringApplication springApplication, ConfigurableApplicationContext context, String... args) {
+        String messagePattern =
+                "SpringApplication: " +
+                        "    main class : '{}' ," +
+                        "    web type : '{}' ," +
+                        "    sources : {} ," +
+                        "    all sources : {} ," +
+                        "    additional profiles : {} ," +
+                        "    initializers : {} ," +
+                        "    listeners : {}," +
+                        "    args : {}," +
+                        "    context id : '{}'";
+
+        Object[] arguments = ofArray(springApplication.getMainApplicationClass(),
+                springApplication.getWebApplicationType(),
+                springApplication.getSources(),
+                springApplication.getAllSources(),
+                springApplication.getAdditionalProfiles(),
+                springApplication.getInitializers(),
+                springApplication.getListeners(),
+                arrayToString(args),
+                context.getId());
+
+        String level = getLoggingLevel(context);
+
+        switch (level) {
+            case "TRACE": {
+                logger.trace(messagePattern, arguments);
+                break;
+            }
+            case "DEBUG": {
+                logger.debug(messagePattern, arguments);
+                break;
+            }
+            case "INFO": {
+                logger.info(messagePattern, arguments);
+                break;
+            }
+            case "WARN": {
+                logger.warn(messagePattern, arguments);
+                break;
+            }
+            case "ERROR": {
+                logger.error(messagePattern, arguments);
+                break;
+            }
+            default: {
+                logger.trace("Logging is off");
+                break;
+            }
+        }
     }
 }
