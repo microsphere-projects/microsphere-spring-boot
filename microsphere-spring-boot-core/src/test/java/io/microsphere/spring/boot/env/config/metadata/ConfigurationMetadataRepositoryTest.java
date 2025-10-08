20afa09bf589b8bb5ee17cee2da9f136536a81da
Mercy Ma <mercyblitz@gmail.com>
2025-10-08 17:12:05 +0800

    Update ConfigurationMetadataRepositoryTest.java

--- microsphere-spring-boot-core/src/test/java/io/microsphere/spring/boot/env/config/metadata/ConfigurationMetadataRepositoryTest.java
+++ microsphere-spring-boot-core/src/test/java/io/microsphere/spring/boot/env/config/metadata/ConfigurationMetadataRepositoryTest.java
@@ -30,8 +30,9 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.AssertionsKt.assertNotNull;
+
 
 /**
  * {@link ConfigurationMetadataRepository} Test
