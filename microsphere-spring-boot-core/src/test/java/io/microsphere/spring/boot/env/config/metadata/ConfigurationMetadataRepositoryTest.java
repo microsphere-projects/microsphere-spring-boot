fc21bbcc960aeb542d95a2cf55f4a7a6c1c4462e
Mercy Ma <mercyblitz@gmail.com>
2025-10-08 19:07:21 +0800

    Add blank line for formatting in test file
    Inserted a blank line after import statements in ConfigurationMetadataRepositoryTest.java to improve code readability and maintain consistent formatting.

--- microsphere-spring-boot-core/src/test/java/io/microsphere/spring/boot/env/config/metadata/ConfigurationMetadataRepositoryTest.java
+++ microsphere-spring-boot-core/src/test/java/io/microsphere/spring/boot/env/config/metadata/ConfigurationMetadataRepositoryTest.java
@@ -33,6 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 /**
  * {@link ConfigurationMetadataRepository} Test
  *
