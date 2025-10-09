7e3f3ed9bb242f786b666eaafee9df352f899f2a
Mercy Ma <mercyblitz@gmail.com>
2025-10-09 13:38:44 +0800

    Refactor default properties loading logic
    Replaces direct use of PropertySourceLoader list with PropertySourceLoaders abstraction, simplifies resource loading, and removes redundant extension matching logic. Logging levels are changed from debug to trace for more granular output. Cleans up unused imports and streamlines the merging of property sources.

--- microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/env/DefaultPropertiesApplicationListener.java
+++ microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/env/DefaultPropertiesApplicationListener.java
@@ -3,8 +3,6 @@ package io.microsphere.spring.boot.env;
 import io.microsphere.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.boot.context.logging.LoggingApplicationListener;
-import org.springframework.boot.env.PropertySourceLoader;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -29,7 +27,6 @@ import static io.microsphere.spring.boot.util.SpringApplicationUtils.getResource
 import static io.microsphere.spring.core.env.PropertySourcesUtils.getDefaultProperties;
 import static io.microsphere.spring.core.io.ResourceLoaderUtils.getResourcePatternResolver;
 import static org.springframework.core.io.support.SpringFactoriesLoader.loadFactories;
-import static org.springframework.util.ObjectUtils.containsElement;
 
 /**
  * Listable {@link ApplicationEnvironmentPreparedEvent} {@link ApplicationListener} Class
@@ -42,7 +39,7 @@ import static org.springframework.util.ObjectUtils.containsElement;
  */
 public class DefaultPropertiesApplicationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, Ordered {
 
-    public static final int DEFAULT_ORDER = LoggingApplicationListener.LOWEST_PRECEDENCE - 1;
+    public static final int DEFAULT_ORDER = LOWEST_PRECEDENCE - 1;
 
     private static final Logger logger = getLogger(DefaultPropertiesApplicationListener.class);
 
@@ -68,7 +65,7 @@ public class DefaultPropertiesApplicationListener implements ApplicationListener
     private void postProcessDefaultProperties(SpringApplication springApplication, Map<String, Object> defaultProperties) {
         ResourceLoader resourceLoader = getResourceLoader(springApplication);
         ClassLoader classLoader = resourceLoader.getClassLoader();
-        List<PropertySourceLoader> propertySourceLoaders = loadFactories(PropertySourceLoader.class, classLoader);
+        PropertySourceLoaders propertySourceLoaders = new PropertySourceLoaders(resourceLoader);
         List<DefaultPropertiesPostProcessor> defaultPropertiesPostProcessors = loadFactories(DefaultPropertiesPostProcessor.class, classLoader);
         // DefaultPropertiesPostProcessor execute
         for (DefaultPropertiesPostProcessor defaultPropertiesPostProcessor : defaultPropertiesPostProcessors) {
@@ -79,42 +76,42 @@ public class DefaultPropertiesApplicationListener implements ApplicationListener
         loadDefaultPropertiesResources(propertySourceLoaders, resourceLoader, defaultProperties);
     }
 
-    private void loadDefaultPropertiesResources(List<PropertySourceLoader> propertySourceLoaders,
+    private void loadDefaultPropertiesResources(PropertySourceLoaders propertySourceLoaders,
                                                 ResourceLoader resourceLoader,
                                                 Map<String, Object> defaultProperties) {
         Set<String> defaultPropertiesResources = getDefaultPropertiesResources();
-        logger.debug("Start loading from SpringApplicationUtils.loadDefaultPropertiesResources() 'defaultProperties resources: {}", defaultPropertiesResources);
+        logger.trace("Start loading from SpringApplicationUtils.loadDefaultPropertiesResources() 'defaultProperties resources: {}", defaultPropertiesResources);
         loadDefaultProperties(defaultPropertiesResources, propertySourceLoaders, resourceLoader, defaultProperties);
     }
 
     private void postProcessDefaultProperties(DefaultPropertiesPostProcessor defaultPropertiesPostProcessor,
-                                              List<PropertySourceLoader> propertySourceLoaders,
+                                              PropertySourceLoaders propertySourceLoaders,
                                               ResourceLoader resourceLoader,
                                               Map<String, Object> defaultProperties) {
         Set<String> defaultPropertiesResources = new LinkedHashSet<>();
 
         String processorClassName = defaultPropertiesPostProcessor.getClass().getName();
 
-        logger.debug("DefaultPropertiesPostProcessor '{}' start processing 'defaultProperties: {}", processorClassName, defaultPropertiesResources);
+        logger.trace("DefaultPropertiesPostProcessor '{}' start processing 'defaultProperties: {}", processorClassName, defaultPropertiesResources);
         defaultPropertiesPostProcessor.initializeResources(defaultPropertiesResources);
 
         // load "defaultProperties"
         loadDefaultProperties(defaultPropertiesResources, propertySourceLoaders, resourceLoader, defaultProperties);
 
         defaultPropertiesPostProcessor.postProcess(defaultProperties);
-        logger.debug("DefaultPropertiesPostProcessor '{}' end processing 'defaultProperties: {}", processorClassName, defaultPropertiesResources);
+        logger.trace("DefaultPropertiesPostProcessor '{}' end processing 'defaultProperties: {}", processorClassName, defaultPropertiesResources);
     }
 
     private void loadDefaultProperties(Collection<String> defaultPropertiesResources,
-                                       List<PropertySourceLoader> propertySourceLoaders,
+                                       PropertySourceLoaders propertySourceLoaders,
                                        ResourceLoader resourceLoader,
                                        Map<String, Object> defaultProperties) {
-        logger.debug("Start loading the 'defaultProperties' resource path list: {}", defaultPropertiesResources);
+        logger.trace("Start loading the 'defaultProperties' resource path list: {}", defaultPropertiesResources);
         ResourcePatternResolver resourcePatternResolver = getResourcePatternResolver(resourceLoader);
         for (String defaultPropertiesResource : defaultPropertiesResources) {
             try {
                 for (Resource resource : resourcePatternResolver.getResources(defaultPropertiesResource)) {
-                    if (!loadDefaultProperties(defaultPropertiesResource, resource, propertySourceLoaders, defaultProperties)) {
+                    if (!loadDefaultProperties(resource, propertySourceLoaders, defaultProperties)) {
                         logger.warn("'defaultProperties' resource [location: {}] failed to load, please confirm the resource can be processed!", resource.getURL());
                     }
                 }
@@ -124,54 +121,30 @@ public class DefaultPropertiesApplicationListener implements ApplicationListener
         }
     }
 
-    private boolean loadDefaultProperties(String defaultPropertiesResource, Resource resource,
-                                          List<PropertySourceLoader> propertySourceLoaders,
+    private boolean loadDefaultProperties(Resource resource, PropertySourceLoaders propertySourceLoaders,
                                           Map<String, Object> defaultProperties) throws IOException {
         boolean loaded = false;
-        for (PropertySourceLoader propertySourceLoader : propertySourceLoaders) {
-            if (loadDefaultProperties(defaultPropertiesResource, resource, propertySourceLoader, defaultProperties)) {
+        URL url = resource.getURL();
+        String resourceLocation = url.getPath();
+        List<PropertySource<?>> propertySources = propertySourceLoaders.load(resourceLocation, resource);
+        logger.trace("'defaultProperties' resource [location: {}] loads into {} PropertySource", resourceLocation, propertySources.size());
+        for (PropertySource propertySource : propertySources) {
+            if (propertySource instanceof EnumerablePropertySource) {
+                merge((EnumerablePropertySource) propertySource, defaultProperties);
                 loaded = true;
-                break;
             }
-        }
-        return loaded;
-    }
-
-    private boolean loadDefaultProperties(String defaultPropertiesResource, Resource resource,
-                                          PropertySourceLoader propertySourceLoader,
-                                          Map<String, Object> defaultProperties) {
-        boolean loaded = false;
-        try {
-            URL url = resource.getURL();
-            String resourceLocation = url.getPath();
-            String fileExtension = getExtension(resourceLocation);
-            String[] fileExtensions = propertySourceLoader.getFileExtensions();
-            if (matches(fileExtension, fileExtensions)) {
-
-                List<PropertySource<?>> propertySources = propertySourceLoader.load(resourceLocation, resource);
-                logger.debug("'defaultProperties' resource [location: {}] loads into {} PropertySource", resourceLocation, propertySources.size());
-                for (PropertySource propertySource : propertySources) {
-                    if (propertySource instanceof EnumerablePropertySource) {
-                        merge((EnumerablePropertySource) propertySource, defaultProperties);
-                        loaded = true;
-                    }
-                }
-
-            }
-        } catch (IOException e) {
-            logger.error("'defaultProperties' resource [{}] failed to load", defaultPropertiesResource, e);
         }
         return loaded;
     }
 
     private void merge(EnumerablePropertySource<?> propertySource, Map<String, Object> defaultProperties) {
-        logger.debug("'defaultProperties' PropertySource[{}] Try merging!", propertySource);
+        logger.trace("'defaultProperties' PropertySource[{}] tries to merge!", propertySource);
         String[] propertyNames = propertySource.getPropertyNames();
         for (String propertyName : propertyNames) {
             Object propertyValue = propertySource.getProperty(propertyName);
             Object oldPropertyValue = defaultProperties.putIfAbsent(propertyName, propertyValue);
             if (oldPropertyValue == null) {
-                logger.debug("'defaultProperties' attribute [name: {}, value: {}] added successfully!", propertyName, propertyValue);
+                logger.trace("'defaultProperties' attribute [name: {}, value: {}] added successfully!", propertyName, propertyValue);
             } else {
                 logger.warn("'defaultProperties' attribute [name: {}, old-value: {}] already exists, new-value[{}] will not be merged!",
                         propertyName, oldPropertyValue, propertyValue);
@@ -179,26 +152,12 @@ public class DefaultPropertiesApplicationListener implements ApplicationListener
         }
     }
 
-    private boolean matches(String fileExtension, String[] fileExtensions) {
-        return containsElement(fileExtensions, fileExtension);
-    }
-
-    private String getExtension(String resourceLocation) {
-        int index = resourceLocation.lastIndexOf(".");
-        if (index == -1) {
-            return null;
-        }
-        String extension = resourceLocation.substring(index + 1);
-        return extension;
-    }
-
     private void logDefaultProperties(SpringApplication springApplication, Map<String, Object> defaultProperties) {
-        logger.debug("SpringApplication[sources:{}] defaultProperties:", springApplication.getSources());
+        logger.trace("SpringApplication[sources:{}] defaultProperties:", springApplication.getSources());
         defaultProperties.forEach((key, value) -> {
-            logger.debug("'{}' = {}", key, value);
+            logger.trace("'{}' = {}", key, value);
         });
     }
-
 
     @Override
     public int getOrder() {
