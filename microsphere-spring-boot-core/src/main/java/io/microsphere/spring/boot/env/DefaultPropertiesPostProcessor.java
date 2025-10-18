c277d2d3c9b1e79448ccf524a31ab641be8edd26
Mercy Ma <mercyblitz@gmail.com>
2025-10-18 17:33:58 +0800

    Update DefaultPropertiesPostProcessor.java

--- microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/env/DefaultPropertiesPostProcessor.java
+++ microsphere-spring-boot-core/src/main/java/io/microsphere/spring/boot/env/DefaultPropertiesPostProcessor.java
@@ -8,7 +8,33 @@ import java.util.Properties;
 import java.util.Set;
 
 /**
- * {@link SpringApplication#setDefaultProperties(Properties) "defaultProperties"} Post Processor
+ * {@link SpringApplication#setDefaultProperties(Properties) "defaultProperties"} Post Processor.
+ *
+ * <h3>Example Usage</h3>
+ * <p>Example usage in a custom {@link DefaultPropertiesPostProcessor}:</p>
+ *
+ * <pre>{@code
+ * public class CustomDefaultPropertiesPostProcessor implements DefaultPropertiesPostProcessor {
+ *
+ *     @Override
+ *     public void initializeResources(Set<String> defaultPropertiesResources) {
+ *         defaultPropertiesResources.add("classpath*:META-INF/custom-default.properties");
+ *     }
+ *
+ *     @Override
+ *     public void postProcess(Map<String> defaultProperties) {
+ *         // Add or modify default properties
+ *         defaultProperties.put("custom.property", "defaultValue");
+ *     }
+ * }
+ * }</pre>
+ *
+ * <p>Register the processor in META-INF/spring.factories:</p>
+ *
+ * <pre>{@code
+ * io.microsphere.spring.boot.env.DefaultPropertiesPostProcessor=\
+ * com.example.CustomDefaultPropertiesPostProcessor
+ * }</pre>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @see DefaultPropertiesApplicationListener
