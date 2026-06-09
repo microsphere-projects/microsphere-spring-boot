1449ac8832ae5d489ec64ee906b5bc4676f30d5d
Mercy Ma <mercyblitz@gmail.com>
2026-06-09 23:38:55 +0800

    Use @EnableWebFluxExtension in test properties
    Update WebFluxAutoConfigurationAllEnabledTest to use the annotation-based property prefix. Replaced
    `microsphere.spring.prefix.EnableWebFluxExtension.*` with
    `microsphere.spring.@EnableWebFluxExtension.*` for storeRequestBodyArgument and storeResponseBodyReturnValue so the test uses the correct property keys.

--- microsphere-spring-boot-webflux/src/test/java/io/microsphere/spring/boot/webflux/autoconfigure/WebFluxAutoConfigurationAllEnabledTest.java
+++ microsphere-spring-boot-webflux/src/test/java/io/microsphere/spring/boot/webflux/autoconfigure/WebFluxAutoConfigurationAllEnabledTest.java
@@ -44,8 +44,8 @@ import static io.microsphere.collection.Sets.ofSet;
 })
 @TestPropertySource(
         properties = {
-                "microsphere.spring.prefix.EnableWebFluxExtension.storeRequestBodyArgument=true",
-                "microsphere.spring.prefix.EnableWebFluxExtension.storeResponseBodyReturnValue=true",
+                "microsphere.spring.@EnableWebFluxExtension.storeRequestBodyArgument=true",
+                "microsphere.spring.@EnableWebFluxExtension.storeResponseBodyReturnValue=true",
         }
 )
 @EnableAutoConfiguration
