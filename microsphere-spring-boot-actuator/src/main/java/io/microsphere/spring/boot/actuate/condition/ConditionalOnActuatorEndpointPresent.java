7a1e8c9d44553704f091590b878edefffea5205a
Mercy Ma <mercyblitz@gmail.com>
2026-07-06 09:09:19 +0800

    Add actuator endpoint presence condition
    Introduce a new `@ConditionalOnActuatorEndpointPresent` annotation in the actuator module. It wraps `@ConditionalOnClass` for `org.springframework.boot.actuate.endpoint.annotation.Endpoint` so configuration can be conditionally enabled only when the Spring Boot Actuator endpoint API is available.

---
+++ microsphere-spring-boot-actuator/src/main/java/io/microsphere/spring/boot/actuate/condition/ConditionalOnActuatorEndpointPresent.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.spring.boot.actuate.condition;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * {@link Conditional @Conditional} that checks whether the Spring Boot Actuator
+ * {@link org.springframework.boot.actuate.endpoint.annotation.Endpoint @Endpoint} API is present
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Conditional
+ * @see org.springframework.boot.actuate.endpoint.annotation.Endpoint
+ * @since 1.0.0
+ */
+@Target({TYPE, METHOD})
+@Retention(RUNTIME)
+@Documented
+@ConditionalOnClass(name = "org.springframework.boot.actuate.endpoint.annotation.Endpoint")
+public @interface ConditionalOnActuatorEndpointPresent {
+}
